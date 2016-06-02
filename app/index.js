06bc02c1d3c15c6184b302470ce6082cba106527
Ahad Wasim <ahadwasim@gmail.com>
2016-06-01 21:11:33 -0700

    add cachedSectors

--- app/index.js
+++ app/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
+import TopBar from './components/top_bar.js';
+import MainContent from './components/main_content.js';
 import './assets/styles/main.css';
 
 const API = "http://data.okfn.org/data/core/s-and-p-500-companies/r/constituents-financials.json";
@@ -9,248 +11,35 @@ class App extends Component {
   constructor(){
     super()
     this.state = {
-      columns: ["Name", "Sector", "MarketCap"]
+      cachedSectors: null,
+      options: null
     }
   }
 
   componentDidMount(){
-    axios.get(API)
-      .then(function(response){
-        var cache = {};
-        response.data.forEach((obj)=> {
-          if(!cache.hasOwnProperty(obj.Sector)){
-            cache[obj.Sector] = true
-          }
-        });
-        console.log(cache);
-        console.log(Object.keys(cache).length);
+    axios.get(API).then((response) => {
+      
+      let cache = {};
+
+      response.data.forEach((company) => {
+        if(!cache[company.Sector]){
+          cache[company.Sector] = [company]
+        } else {
+          cache[company.Sector].push(company)
+        }
       });
+
+      this.setState({cachedSectors: cache})
+
+    });
   }
 
   render(){
     return (
       <div>
-
-        <section className="top-bar">
-          <div id="logo">
-            <img src="https://www.qsensei.com/site/wp-content/themes/qsensei/assets/QS_Logo_white.png" />
-          </div>
-        </section>
-
-
-        <section className="main-content clearfix">
-
-          <section className="left option-wrapper">
-            <div className="searchBar-wrapper">
-              <input className="searchBar" type="text" placeholder="ex: engineering" />
-            </div>
-
-            <div className="sectorBox">
-              <div className="tags">
-                <p>Engineering</p> <span className="close">X</span>
-              </div>
-              <div className="tags">
-                <p>Engineering</p> <span className="close">X</span>
-              </div>
-              <div className="tags">
-                <p>Engineering</p> <span className="close">X</span>
-              </div>
-              <div className="tags">
-                <p>Engineering</p> <span className="close">X</span>
-              </div>
-              <div className="tags">
-                <p>Engineering</p> <span className="close">X</span>
-              </div>
-              <div className="tags">
-                <p>Engineering</p> <span className="close">X</span>
-              </div>
-              <div className="tags">
-                <p>Engineering</p> <span className="close">X</span>
-              </div>
-              <div className="tags">
-                <p>Engineering</p> <span className="close">X</span>
-              </div>
-              <div className="tags">
-                <p>Engineering</p> <span className="close">X</span>
-              </div>
-              <div className="tags">
-                <p>Engineering</p> <span className="close">X</span>
-              </div>
-            </div>
-
-
-            <div className="summary">
-              <p><span>Company Count:</span>24</p>
-              <p><span>Sum:</span>35.103</p>
-            </div>
-
-
-
-          </section>
-
-          <section className="right table-wrapper">
-            
-            <table id="companyData">
-              <thead>
-                <tr>
-                  <th>Name</th>
-                  <th>Sector</th>
-                  <th>Market Cap</th>
-                </tr>
-              </thead>
-
-
-              <tbody>
-                <tr>
-                  <td>1</td>
-                  <td>2</td>
-                  <td>3</td>
-                </tr>
-
-                <tr>
-                  <td>1</td>
-                  <td>2</td>
-                  <td>3</td>
-                </tr>
-                                <tr>
-                  <td>1</td>
-                  <td>2</td>
-                  <td>3</td>
-                </tr>
-
-                <tr>
-                  <td>1</td>
-                  <td>2</td>
-                  <td>3</td>
-                </tr>                <tr>
-                  <td>1</td>
-                  <td>2</td>
-                  <td>3</td>
-                </tr>
-
-                <tr>
-                  <td>1</td>
-                  <td>2</td>
-                  <td>3</td>
-                </tr>                <tr>
-                  <td>1</td>
-                  <td>2</td>
-                  <td>3</td>
-                </tr>
-
-                <tr>
-                  <td>1</td>
-                  <td>2</td>
-                  <td>3</td>
-                </tr>                <tr>
-                  <td>1</td>
-                  <td>2</td>
-                  <td>3</td>
-                </tr>
-
-                <tr>
-                  <td>1</td>
-                  <td>2</td>
-                  <td>3</td>
-                </tr>                <tr>
-                  <td>1</td>
-                  <td>2</td>
-                  <td>3</td>
-                </tr>
-
-                <tr>
-                  <td>1</td>
-                  <td>2</td>
-                  <td>3</td>
-                </tr>                <tr>
-                  <td>1</td>
-                  <td>2</td>
-                  <td>3</td>
-                </tr>
-
-                <tr>
-                  <td>1</td>
-                  <td>2</td>
-                  <td>3</td>
-                </tr>                <tr>
-                  <td>1</td>
-                  <td>2</td>
-                  <td>3</td>
-                </tr>
-
-                <tr>
-                  <td>1</td>
-                  <td>2</td>
-                  <td>3</td>
-                </tr>                <tr>
-                  <td>1</td>
-                  <td>2</td>
-                  <td>3</td>
-                </tr>
-
-                <tr>
-                  <td>1</td>
-                  <td>2</td>
-                  <td>3</td>
-                </tr>                <tr>
-                  <td>1</td>
-                  <td>2</td>
-                  <td>3</td>
-                </tr>
-
-                <tr>
-                  <td>1</td>
-                  <td>2</td>
-                  <td>3</td>
-                </tr>                <tr>
-                  <td>1</td>
-                  <td>2</td>
-                  <td>3</td>
-                </tr>
-
-                <tr>
-                  <td>1</td>
-                  <td>2</td>
-                  <td>3</td>
-                </tr>                <tr>
-                  <td>1</td>
-                  <td>2</td>
-                  <td>3</td>
-                </tr>
-
-                <tr>
-                  <td>1</td>
-                  <td>2</td>
-                  <td>3</td>
-                </tr>                <tr>
-                  <td>1</td>
-                  <td>2</td>
-                  <td>3</td>
-                </tr>
-
-                <tr>
-                  <td>1</td>
-                  <td>2</td>
-                  <td>3</td>
-                </tr>
-              </tbody>
-            </table> 
-          </section>
-
-        </section>
-
-      
-
-
-
-      <div>
-       
-        
+        <TopBar />
+        <MainContent />
       </div>
-
-    </div>
-
-
     );
   }
 }
