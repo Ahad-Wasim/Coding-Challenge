5fef5c3564cb128f5ebfb433237a0c8a47441042
Ahad Wasim <ahadwasim@gmail.com>
2016-06-01 21:24:35 -0700

    create table_body file

---
+++ app/components/table_body.js
@@ -0,0 +1,143 @@
+import React from 'react';
+import Company_Row from './company_row.js';
+
+const TableBody = () => {
+  return (
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
+      </tr>
+
+      <tr>
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
+      </tr>
+                      <tr>
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
+      </tr>
+
+      <tr>
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
+      </tr>                <tr>
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
+      </tr>
+
+      <tr>
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
+      </tr>                <tr>
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
+      </tr>
+
+      <tr>
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
+      </tr>                <tr>
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
+      </tr>
+
+      <tr>
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
+      </tr>                <tr>
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
+      </tr>
+
+      <tr>
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
+      </tr>                <tr>
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
+      </tr>
+
+      <tr>
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
+      </tr>                <tr>
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
+      </tr>
+
+      <tr>
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
+      </tr>                <tr>
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
+      </tr>
+
+      <tr>
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
+      </tr>                <tr>
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
+      </tr>
+
+      <tr>
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
+      </tr>                <tr>
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
+      </tr>
+
+      <tr>
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
+      </tr>                <tr>
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
+      </tr>
+
+      <tr>
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
+      </tr>                <tr>
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
+      </tr>
+
+      <tr>
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
+      </tr>
+    </tbody>
+  );
+};
+
+export default TableBody;
