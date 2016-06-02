79be780d0bc176a693bca3b3ea5ed071fcc22495
Ahad Wasim <ahadwasim@gmail.com>
2016-06-01 21:26:27 -0700

    main_content view complete

--- app/components/main_content.js
+++ app/components/main_content.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import SearchBar from './search_bar.js';
+import OptionWrapper from './option_wrapper.js';
+import TableWrapper from './table_wrapper.js';
 
 const MainContent = () => {
   return (
     <section className="main-content clearfix">
-
+      <OptionWrapper />
+      <TableWrapper />
     </section>
   );
 }
+
+export default MainContent;
