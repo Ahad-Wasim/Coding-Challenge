820b4e0c25b26df06a9d881d4017de0e71f8e8ec
Ahad Wasim <ahadwasim@gmail.com>
2016-06-01 21:41:01 -0700

    sectorBox view complete

--- app/components/sector_Box.js
+++ app/components/sector_Box.js
@@ -1,8 +1,40 @@
 import React from 'react';
 
-
 const SectorBox = () => {
-
+  return (
+    <div className="sectorBox">
+      <div className="tags">
+        <p>Engineering</p> <span className="close">X</span>
+      </div>
+      <div className="tags">
+        <p>Engineering</p> <span className="close">X</span>
+      </div>
+      <div className="tags">
+        <p>Engineering</p> <span className="close">X</span>
+      </div>
+      <div className="tags">
+        <p>Engineering</p> <span className="close">X</span>
+      </div>
+      <div className="tags">
+        <p>Engineering</p> <span className="close">X</span>
+      </div>
+      <div className="tags">
+        <p>Engineering</p> <span className="close">X</span>
+      </div>
+      <div className="tags">
+        <p>Engineering</p> <span className="close">X</span>
+      </div>
+      <div className="tags">
+        <p>Engineering</p> <span className="close">X</span>
+      </div>
+      <div className="tags">
+        <p>Engineering</p> <span className="close">X</span>
+      </div>
+      <div className="tags">
+        <p>Engineering</p> <span className="close">X</span>
+      </div>
+    </div>
+  );
 };
 
 export default SectorBox;
