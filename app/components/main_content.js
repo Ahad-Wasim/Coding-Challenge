156ff5b364ac99cb2eb04163a7a7d2cf43f9014f
Ahad Wasim <ahadwasim@gmail.com>
2016-06-01 19:36:34 -0700

    create main_contents component

---
+++ app/components/main_content.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import SearchBar from './search_bar.js';
+
+const MainContent = () => {
+  return (
+    <section className="main-content clearfix">
+
+    </section>
+  );
+}
