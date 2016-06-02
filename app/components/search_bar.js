090a94ebb5b34b739d083927a8b2633e0ef95881
Ahad Wasim <ahadwasim@gmail.com>
2016-06-01 19:30:57 -0700

    search_bar component instantiated

---
+++ app/components/search_bar.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  constructor(){
+    super()
+    this.state = {
+      searchValue: ''
+    }
+  }
+
+
+  render(){
+    return (
+      <div className="searchBar-wrapper">
+        <input
+          type="text"
+          className="searchBar" 
+          placeholder="ex: engineering" 
+          value={this.state.searchValue}
+          onChange={(e) => this.setState(e.target.value)} 
+        />
+      </div>
+    );
+  }
+}
