bec2ad5bcd4b4e34df86d0054f243d57fd80f752
Ahad Wasim <ahadwasim@gmail.com>
2016-06-01 21:28:54 -0700

    option wrapper now holds SearchBar, SectorBox, Summary

--- app/components/option_wrapper.js
+++ app/components/option_wrapper.js
@@ -1,2 +1,20 @@
 import React from 'react';
+import SearchBar from './search_bar.js';
+import SectorBox from './sector_box.js';
+import Summary from './summary.js';
+
+
+const OptionWrapper = () => {
+  return (
+    <section className="left option-wrapper">
+      <SearchBar />
+      <SectorBox />
+      <Summary />
+    </section>
+  );
+}
+
+
+export default OptionWrapper;
+
 
