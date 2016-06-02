0ab614c9cb66f725ec85adc2432572b3c61093f4
Ahad Wasim <ahadwasim@gmail.com>
2016-06-01 18:48:12 -0700

    topbar complete

---
+++ app/components/top_bar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const TopBar = () => {
+  return (
+    <section className="top-bar">
+      <div id="logo">
+        <img src="https://www.qsensei.com/site/wp-content/themes/qsensei/assets/QS_Logo_white.png" />
+      </div>
+    </section>
+  );
+}
+
+export default TopBar
