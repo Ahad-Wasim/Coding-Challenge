dc84d1032f1d2ca2b2d8d4ce80178c07da3388ae
Ahad Wasim <ahadwasim@gmail.com>
2016-06-01 21:24:15 -0700

    create summary components file

---
+++ app/components/summary.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Summary = () => {
+  return (
+    <div className="summary">
+      <p><span>Company Count:</span>24</p>
+      <p><span>Sum:</span>35.103</p>
+    </div>
+  );
+}
+
+export default Summary;
